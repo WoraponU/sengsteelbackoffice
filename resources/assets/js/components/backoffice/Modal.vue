b96d1d80f7e4736aec379664ef7fafa309e2b136
worapon <woraponok@outlook.com>
2017-01-28 12:40:38 +0700

    add photo finished

--- resources/assets/js/components/backoffice/Modal.vue
+++ resources/assets/js/components/backoffice/Modal.vue
@@ -3,15 +3,22 @@
         <div class="modal-content">
             <h4>User Infomation</h4>
             <div class="row">
+                <div class="col s12 m12 l12 center-align">
+                    <img class="circle responsive-img" :src="imgIcon" alt="photo">
+                    <input @change="onImageChange($event)" type="file" name="pic" accept="image/*">
+                </div>
+            </div>
+            <div class="row">
                 <div class="input-field col s12 m12 l6">
-                    <input @change="test" id="firstname" type="text" class="validate">
+                    <input id="firstname" type="text" class="validate">
                     <label for="firstname">First Name <span class="icon-star">*</span></label>
-                    
                 </div>
                 <div class="input-field col s12 m12 l6">
                     <input id="lastname" type="text" class="validate">
                     <label for="lastname">Last Name <span class="icon-star">*</span></label>
                 </div>
+            </div>
+            <div class="row">
                 <div class="input-field col s12 m12 l6">
                     <input id="identificationNumber" type="text" class="validate">
                     <label for="identificationNumber">Identification Number <span class="icon-star">*</span></label>
@@ -20,35 +27,36 @@
                     <input id="email" type="email" class="validate">
                     <label for="email" data-error="wrong format">Email</label>
                 </div>
-
-                <br>
+            </div>
+            <div class="row">
                 <div class="input-field col s12 m12 l12">
                     <textarea id="address" class="materialize-textarea"></textarea>
                     <label for="address">Address</label>
                 </div>
-
-                <br>
-                <div class="input-field col s12 m12 l6">
-                    <select @change="test">
-                        <option value="" disabled selected>Choose Role</option>
-                        <option value="admin">Admin</option>
-                        <option value="staff">Staff</option>
-                        <option value="truckDriver">Truck Driver</option>
+            </div>
+            <div class="row">
+                <div class="col s12 m12 l6">
+                    <label class="role-label">Role <span class="icon-star">*</span></label>
+                    <select class="browser-default" v-model="selected" @change="onRoleChange">
+                        <option v-for="option in options" v-bind:value="option.value" :disabled="option.disabled">
+                            {{ option.text }}
+                        </option>
                     </select>
-                    <label>Role <span class="icon-star">*</span></label>
                 </div>
-                <div class="input-field col s12 m12 l6" v-show="dataShow">
+            </div>
+            <div class="row" v-show="isShowUserPass">
+                <div class="input-field col s12 m12 l6" >
                     <input id="username" type="text" class="validate">
                     <label for="username">Username</label>
                 </div>
-                <div class="input-field col s12 m12 l6" v-show="dataShow">
+                <div class="input-field col s12 m12 l6">
                     <input id="password" type="password" class="validate">
                     <label for="password">Password</label>
                 </div>
             </div>
         </div>
         <div class="modal-footer">
-            <a @click="test" href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
             <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Cancel</a>
         </div>
     </div>
@@ -60,38 +68,57 @@
     export default {
         data() {
             return {
-                dataShow: false
+                isShowUserPass: false,
+                selected: '',
+                imgIconValue: '',
+                options: [
+                    { text: 'Choose your option', value: '', disabled: true },
+                    { text: 'Owner', value: 'owner' },
+                    { text: 'Admin', value: 'admin' },
+                    { text: 'Truck Driver', value: 'truckDriver' }
+                ],
+                imgIcon: '../img/user_image.png'
             }
         },
         methods: {
-            addUser() {
-                axios.post('/user', {
-                    firstName: 'Fred',
-                    lastName: 'Flintstone'
-                })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+            onImageChange(e) {
+                var files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                this.createImage(files[0]);
             },
-            test() {
-                alert();
-            }
+            createImage(file) {
+                var image = new Image();
+                var reader = new FileReader();
+
+                reader.onload = (e) => {
+                    this.imgIcon = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            },
+            onRoleChange() {
+                if (this.selected == 'truckDriver') {
+                    this.isShowUserPass = false
+
+                } else {
+                    this.isShowUserPass = true
+                }
+            },
+            onAddUser() {
+                const params = {
+                    // firstname: ,
+                };
+                // axios.post('/user', {
+                //     firstname: ,
+                //     lastName: 'Flintstone'
+                // })
+                // .then(function (response) {
+                //     console.log(response);
+                // })
+                // .catch(function (error) {
+                //     console.log(error);
+                // });
+            },
         }
-        // props: {
-        //     name: { require: true },
-        //     selected: { default: false }
-        // },
-        
-        // computed: {
-        //     href() {
-        //         return '#' + this.name.toLowerCase().replace(/ /g, '-');
-        //     }
-        // },
-        // mounted() {
-        //     this.isActive = this.selected
-        // }
     }
 </script>
