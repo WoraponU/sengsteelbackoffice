c8482f790acf64756a1e6bbac528508dddf48ee6
worapon <woraponok@outlook.com>
2017-02-20 17:16:19 +0700

    swith tab back and front

--- resources/assets/js/components/backoffice/NavBar.vue
+++ resources/assets/js/components/backoffice/NavBar.vue
@@ -5,6 +5,11 @@
                 <a href="/backoffice" class="brand-logo">Seng Backoffice</a>
                 <ul class="right">
                     <li class="logout cyan darken-2">
+                        <a href="/" class="waves-effect waves-block waves-light">
+                            <i class="material-icons">ev_station</i>
+                        </a>
+                    </li>
+                    <li class="logout cyan darken-2">
                         <a href="/logout" class="waves-effect waves-block waves-light">
                             <i class="Large material-icons">power_settings_new</i>
                         </a>
--- resources/assets/js/components/frontoffice/NavBar.vue
+++ resources/assets/js/components/frontoffice/NavBar.vue
@@ -5,6 +5,11 @@
                 <a href="/" class="brand-logo">Seng Backoffice</a>
                 <ul class="right">
                     <li class="logout cyan darken-2">
+                        <a href="/backoffice" class="waves-effect waves-block waves-light">
+                            <i class="material-icons">group_add</i>
+                        </a>
+                    </li>
+                    <li class="logout cyan darken-2">
                         <a href="/logout" class="waves-effect waves-block waves-light">
                             <i class="Large material-icons">power_settings_new</i>
                         </a>
@@ -14,7 +19,6 @@
         </nav>
     </div>
 </template>
-
 <script>
     export default {
     }
