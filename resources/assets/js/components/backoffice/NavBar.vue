9b886a87abbba4beb749ed5aede8bbfce2663548
admin <worapon.p@hobbiz.co.th>
2017-08-07 09:26:35 +0700

    แก้สีกับชื่อ

--- resources/assets/js/components/backoffice/NavBar.vue
+++ resources/assets/js/components/backoffice/NavBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="navbar-fixed">
         <nav>
-            <div class="nav-wrapper cyan darken-3">
-                <a href="/backoffice" class="brand-logo">Seng Backoffice</a>
+            <div class="nav-wrapper deep-orange darken-3">
+                <a href="/backoffice" class="brand-logo">Seng Steel Backoffice</a>
                 <ul class="right">
-                    <li class="logout cyan darken-2">
+                    <li class="logout deep-orange darken-4">
                         <a href="/" class="waves-effect waves-block waves-light">
                             <i class="material-icons">ev_station</i>
                         </a>
                     </li>
-                    <li class="logout cyan darken-2">
+                    <li class="logout deep-orange darken-4">
                         <a href="/logout" class="waves-effect waves-block waves-light">
                             <i class="Large material-icons">power_settings_new</i>
                         </a>
--- resources/assets/js/components/frontoffice/NavBar.vue
+++ resources/assets/js/components/frontoffice/NavBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="navbar-fixed">
         <nav>
-            <div class="nav-wrapper cyan darken-3">
-                <a href="/" class="brand-logo">Seng Backoffice</a>
+            <div class="nav-wrapper deep-orange darken-3">
+                <a href="/" class="brand-logo">Seng Steel Backoffice</a>
                 <ul class="right">
-                    <li class="logout cyan darken-2">
+                    <li class="logout deep-orange darken-4">
                         <a href="/backoffice" class="waves-effect waves-block waves-light">
                             <i class="material-icons">group_add</i>
                         </a>
                     </li>
-                    <li class="logout cyan darken-2">
+                    <li class="logout deep-orange darken-4">
                         <a href="/logout" class="waves-effect waves-block waves-light">
                             <i class="Large material-icons">power_settings_new</i>
                         </a>
